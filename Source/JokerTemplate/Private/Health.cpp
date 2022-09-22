fd9446fb2a16d7fc9765b20c6b65c944a77b8f8c
max2556 <46759632+max2556@users.noreply.github.com>
2022-09-22 16:16:56 +0300

    Add simple enemy and health system

---
+++ Source/JokerTemplate/Private/Health.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Health.h"
+
+// Sets default values for this component's properties
+UHealth::UHealth()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+// Called when the game starts
+void UHealth::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+// Called every frame
+void UHealth::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+
+int UHealth::GetHealth() {
+	return health;
+}
+int UHealth::GetMaxHealth() {
+	return MAX_HEALTH;
+}
+void UHealth::SetHealth(int p_value) {
+	health = p_value;
+}
+void UHealth::ChangeHealth(int p_delta) {
+	SetHealth(GetHealth() + p_delta);
+}
+void UHealth::TakeDamage(int p_delta) {
+	ChangeHealth(p_delta * -1);
+}
+void UHealth::TakeHeal(int p_delta) {
+	ChangeHealth(p_delta);
+}
+
---
+++ Source/JokerTemplate/Public/Health.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Health.generated.h"
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class JOKERTEMPLATE_API UHealth : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UHealth();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	//^^^^^^^^^^^^^^^^^^^^^^^^
+	//Вот это от анрила
+
+
+
+	//ТУТ МОЁ
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int health = 100; //Текущее здоровье
+	UPROPERTY(EditAnywhere)
+		int MAX_HEALTH = 100; //Максимальное здоровье
+	bool isDead = false; //Живы ли мы
+public:
+	UFUNCTION(BlueprintPure) int GetHealth(); //Возвращает текущее здоровье
+	UFUNCTION(BlueprintPure) int GetMaxHealth(); //Возвращает максимальное здоровье
+
+	UFUNCTION(BlueprintCallable) void SetHealth(int NewHealth); //Устанавливает значение здоровья
+	UFUNCTION(BlueprintCallable) void ChangeHealth(int p_delta); //Меняет здоровье
+	UFUNCTION(BlueprintCallable) void TakeDamage(int Damage); //Меняет здоровье(получение урона)
+	UFUNCTION(BlueprintCallable) void TakeHeal(int Heal); //Меняет здоровье(восстановление)
+};
+
+/*
+float ratio = 0.23; //23%
+
+//Green
+int colors[] = { 0, 0.2, 0.4, 0.6, 0.8, 1 };
+
+int main() {
+	if (ratio < 0.2) std::cout << "1";
+	else if (ratio < 0.4) std::cout << "2";
+	else if (ratio < 0.4) std::cout << "2";
+	else if (ratio < 0.4) std::cout << "2";
+	else if (ratio < 0.4) std::cout << "2";
+}
+*/
