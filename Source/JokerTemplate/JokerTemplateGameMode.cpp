271bf6dd37174d43c306caead5f9eece0b803910
Don-Smilo <don_smilo@mail.ru>
2022-12-21 21:58:24 +0300

    настроил управление
    прыгает, бегает, поворачивается по нажатию лкм

--- Source/JokerTemplate/JokerTemplateGameMode.cpp
+++ Source/JokerTemplate/JokerTemplateGameMode.cpp
@@ -1,15 +1,26 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "JokerTemplateGameMode.h"
+#include "JokerTemplateController.h"
 #include "JokerTemplateCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AJokerTemplateGameMode::AJokerTemplateGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	// Установка кастомного класса контроллера
+	PlayerControllerClass = AJokerTemplateController::StaticClass();
+
+	// Установка дефолтного pawn класса блюпринту персонажа
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_JokerTemplateCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	// Установка дефолтного контроллера блюпринту контроллера
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_JokerTemplateController"));
+	if (PlayerControllerBPClass.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
 	}
 }
